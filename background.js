fc9c9f1fc5b6acc5d21cee47f30efeea988185ec
Conanap <albionapc@gmail.com>
2018-07-23 11:51:31 -0400

    Init commit

---
+++ background.js
@@ -0,0 +1,25 @@
+'use strict';
+
+console.log("NASI loaded.");
+
+// prereq functions
+function sleep(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+};
+
+async function check() {
+  while(true) {
+      while(document.getElementsByClassName("skip-credits").length == 0) {
+        await sleep(1000);
+      }
+
+      await sleep(1000);
+      if($(".skip-credits")[0])
+        $(".skip-credits")[0].children[0].click();
+
+    console.log("Skipped.");
+    await(400000);
+  }
+};
+
+check();
